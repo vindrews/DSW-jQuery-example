40849bce218e16ecdf8cb028b31c34dd705323aa
Sky Adams <skyadams@sbunified.org>
2017-08-30 17:33:33 -0700

    moved js to external file

---
+++ myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	   $("h1").click(function() {
+		$("p").css({"color":"red", "background-color": "yellow"});
+		$("img").css("border","25px solid black");
+	   });
+	 });
