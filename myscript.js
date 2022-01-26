d62f7a6beef417d0ba26031b0a78513c91033407
P1 <you@example.com>
2022-01-26 09:46:57 -0800

    made paragraphs dissapear, added alerts when cursor moves over image

--- myscript.js
+++ myscript.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
+		
+	$("#p1").click(function(){
+		$(".disappear").toggle();
+	});
+	
+	$("#boat").mouseenter(function(){
+		alert("Good job! You successfully toggled the sailboat and moved your cursor over it!");
+	});
+
+	$("#boat").mouseleave(function(){
+		alert("Even better job! You moved your cursor away from the image to close that alert!");
+	});
 });
